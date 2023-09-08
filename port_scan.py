4fe8811337c644581305b6b6b85f24d305149eae
DerekHaber <144399414+DerekHaber@users.noreply.github.com>
2023-09-07 20:05:26 -0500

    Update port_scan.py

--- port_scan.py
+++ port_scan.py
@@ -16,3 +16,12 @@ resp = input("""\nScan type:
              2) UDP
              3) Comprehensive""")
 print('Starting ', resp)
+
+# Setting up Syn Ack scan
+if resp == "1":
+    print('Nmap version: ', scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS') # Starts Syn Ack scan on ports 1-1024
+    print(scanner.scaninfo()) # Prints scan info
+    print('IP Up? ', scanner[ip_addr].state()) # States whether provided IP Address is running
+    print(scanner[ip_addr].all_protocols()) # Port type? TCP or UDP
+    print('Open Ports: ', scanner[ip_addr]['tcp'].keys())
